8ec71834f108aff379e7acaefbb1c4d6d964d15a
Daniel <widowmaker58@live.ca>
2020-03-04 02:28:56 -0500

    Commiting work done so far for express server
    Backend code for routing paths for get and post apis. Connects to mongodb via mongoose

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose, { Schema } from 'mongoose';
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const todoSchema = new mongoose.Schema({
+    todo_description: String,
+    todo_responsible: String,
+    todo_priority: String,
+    todo_completed: Boolean
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+    console.log("MongoDB database connection established successfully!");
+})
+
+const TodoModel = mongoose.model('Todo', todoSchema);
+
+module.exports = TodoModel;
+
+const todoRoutes = express.Router();
+
+todoRoutes.route('/').get((req, res) => {
+    TodoModel.find({}, function (err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function (req, res) {
+    let id = req.params.id;
+    TodoModel.findById(id, function (err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/add').post(function (req, res) {
+    let todo = new TodoModel(req.body);
+    console.log(`Got a create request for ${JSON.stringify(req.body)}`);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({ 'todo': 'todo added successfully' });
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+todoRoutes.route('/update/:id').post(function (req, res) {
+    TodoModel.findById(req.params.id, function (err, todo) {
+        if (!todo) {
+            res.status(404).send("data is not found");
+        } else {
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+                .catch(err => {
+                    res.status(400).send("Update not possible");
+                });
+        }
+    });
+});
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function () {
+    console.log("Server is running on Port with: " + PORT);
+});
